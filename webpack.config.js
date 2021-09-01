fac287c594b8d48fe7a341a1d20b0ca650b29f98
Sam Brown <baudot@gmail.com>
2021-08-31 17:40:43 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  watch: true,
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
