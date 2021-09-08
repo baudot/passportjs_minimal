7a9a3025bcadadf0ddbe6dd944a4239b0ccd47f1
Sam Brown <baudot@gmail.com>
2021-09-07 19:18:57 -0700

    DB update, etc.

--- db/setup.sql
+++ db/setup.sql
@@ -1,10 +1,11 @@
+-- final version: Make eMail constrained to be unique. For early testing, easier not to.
+
 CREATE TABLE users (
   id uuid not null primary key,
-  password varchar(64) not null,
-  salt varchar(64) not null,
+  password varchar(64) not null, -- bcrypt stores hashed password together with its salt.
   first_name varchar(64) not null,
   last_name varchar(64) not null,
-  email varchar(64) not null,
+  email varchar(64) not null unique,
   cash_position bigint default 1000000
 );
 
@@ -14,6 +15,6 @@ CREATE INDEX ON users (email);
 CREATE INDEX ON users (cash_position);
 
 INSERT INTO users
-(id, password, salt, first_name, last_name, email, cash_position)
+(id, password, first_name, last_name, email, cash_position)
 VALUES
-('A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11', 'foo', 'bar', 'john', 'smith', 'john_smith@example.com', 1000000)
+('A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11', 'foo', 'john', 'smith', 'john_smith@example.com', 1000000)
